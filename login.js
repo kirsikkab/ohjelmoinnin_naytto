edc8230e20f6ad6306bccf7cac0d1e54faa1355a
VH <114520232+Bananasauce23@users.noreply.github.com>
2025-01-08 13:45:11 +0200

    kirjautuminen + rekisteröinti
    - kirjautuminen ja rekisteröinti onnistuu, lisää userList -listaan
    - kun Nettikirppis -tekstiä klikkaa, päätyy etusivulle

---
+++ login.js
@@ -0,0 +1,85 @@
+if (localStorage.getItem("admins") == null){
+    localStorage.setItem("admins", JSON.stringify({}))
+}
+let admins = JSON.parse(localStorage.getItem("admins"))
+
+if (localStorage.getItem("userList") == null){
+    localStorage.setItem("userList", JSON.stringify({}))
+}
+let userList = JSON.parse(localStorage.getItem("userList"))
+
+let normalUsers = {}
+let name = ""
+
+// Lisää uuden käyttäjän
+function addUser(){
+    let error = false
+    name = document.getElementById("new-name").value
+    let password = document.getElementById("new-password").value
+
+    errors(name, password)
+
+    // Tarkastaa, onko käyttäjälista tyhjä, ja onko nimi käytössä
+    if (Object.keys(userList).length == 0){
+        error = false
+    }
+    else{
+        for (x in userList){
+            if (name == x){
+                alert("Käyttäjänimi on jo käytössä")
+                error = true
+            }
+        }
+    }
+
+    // Lisää käyttäjän käyttäjälistaan + tavalliset käyttäjät/ylläpitäjät -listaan
+    if (error == false && name != "" && password != ""){
+        if (document.getElementById("normal-user").checked){
+            normalUsers[name] = password
+            localStorage.setItem("normalUsers", JSON.stringify(normalUsers))
+        }
+        else{
+            admins[name] = password
+            localStorage.setItem("admins", JSON.stringify(admins))
+        }
+        userList[name] = password 
+        localStorage.setItem("userList", JSON.stringify(userList))
+    }
+}
+
+// Näyttää virheet
+function errors(name, password){
+    if (name == "" || password == ""){
+        if (name == "" && password == ""){
+            alert("Anna käyttäjänimi ja salasana")
+        }
+        else if (name == ""){
+            alert("Anna käyttäjänimi")
+        }
+        else if (password == ""){
+            alert("Anna salasana")
+        }
+    }
+}
+
+// Tarkistaa onko käyttäjänimen salasana oikein
+function checkInfo(){
+    name = document.getElementById("registered-username").value
+    let password = document.getElementById("registered-password").value
+
+    errors(name, password)
+
+    if (name in userList && Object.keys(userList).length != 0){
+        if (password != userList[name]){
+            alert("Väärä salasana, yritä uudelleen")
+        }
+        else{
+            window.localStorage.setItem("admins", JSON.stringify(admins))
+            localStorage.setItem("name", name)
+            window.location.replace("index.html")
+        }
+    }
+    else{
+        alert("Käyttäjänimeä ei löytynyt")
+    }
+}
