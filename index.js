8ee58eff4f4d5c838d9ed8587c1eb83f7c8988be
kirsikkab <kirsikka.bister@gmail.com>
2025-01-13 18:46:40 +0200

    uuden ilmoituksen luonti

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Kirjautumisnappi
 function clickLogin(){
     window.location.replace("login.html")
 }
@@ -12,44 +11,3 @@ function backToMain(){
 function createNewListing() {
     window.location.replace("newListing.html")
 }
-
-// Myynti-ilmoitusten luonti etusivulle
-document.addEventListener("DOMContentLoaded", function () {
-    const listingsContainer = document.getElementById("listings");
-
-    // Hae localStoragesta tallennetut ilmoitukset
-    const listings = JSON.parse(localStorage.getItem("listings")) || [];
-
-    // Luo HTML-ilmoitukset
-    listings.forEach((listing) => {
-        const listingElement = document.createElement("div");
-        listingElement.className = "listing d-flex flex-column justify-content-center";
-
-        listingElement.innerHTML = `
-            <h2 class="h2-listing">${listing.title}</h2>
-            <h5 class="h5-listing">${listing.category}</h5>
-            <p class="listing-description">${listing.description}</p>
-            ${
-                listing.isAuction
-                    ? `<p class="listing-price">Lähtöhinta: <span class="start-price">${listing.price.toFixed(
-                          2
-                      )}</span> €</p>
-                       <p class="closing-date">Huutokauppa sulkeutuu: <span class="date">${new Date(
-                           listing.auctionEnd
-                       ).toLocaleString()}</span></p>`
-                    : `<p class="listing-price"><span class="price">${listing.price.toFixed(
-                          2
-                      )}</span> €</p>`
-            }
-            <div class="btns-listing d-flex justify-content-center pt-3">
-                ${
-                    listing.isAuction
-                        ? `<button class="btn-bid theme1">Tarjoa hintaa</button>`
-                        : `<button class="btn-send-message theme1">Lähetä viesti myyjälle</button>`
-                }
-            </div>
-        `;
-
-        listingsContainer.appendChild(listingElement);
-    });
-});
