bf15f6a03bb073ec704e0dd183e3d72ac8796d6b
VH <114520232+Bananasauce23@users.noreply.github.com>
2025-01-15 13:47:20 +0200

    Sähköposti- ja paikkakuntakentät
    - lisätty sähköposti- ja paikkakuntakentät
    - muokattu nappeja

--- login.js
+++ login.js
@@ -16,8 +16,10 @@ function addUser(){
     let error = false
     name = document.getElementById("new-name").value
     let password = document.getElementById("new-password").value
+    let email = document.getElementById("new-email").value
+    let locality = document.getElementById("new-locality").value
 
-    errors(name, password)
+    errors(name, password, email, locality)
 
     // Tarkastaa, onko userList tyhjä, ja onko nimi jo rekisteröity
     if (Object.keys(userList).length == 0){
@@ -33,32 +35,41 @@ function addUser(){
     }
 
     // Lisää käyttäjän userListiin ja tavalliset käyttäjät/ylläpitäjät -listaan
-    if (error == false && name != "" && password != ""){
+    if (error == false && name != "" && password != "" && email != "" && locality != ""){
         if (document.getElementById("normal-user").checked){
-            normalUsers[name] = password
+            normalUsers[name] = [password, email, locality]
             localStorage.setItem("normalUsers", JSON.stringify(normalUsers))
         }
         else{
-            admins[name] = password
+            admins[name] = [password, email, locality]
             localStorage.setItem("admins", JSON.stringify(admins))
         }
-        userList[name] = password 
+        userList[name] = [password, email, locality]
         localStorage.setItem("userList", JSON.stringify(userList))
+        document.getElementById("new-name").value = ""
+        document.getElementById("new-password").value = ""
+        document.getElementById("new-email").value = ""
+        document.getElementById("new-locality").value = ""
     }
 }
 
-// Näyttää virheet, jos käyttäjänimi- ja/tai salasanakentässä ei ole mitään
-function errors(name, password){
-    if (name == "" || password == ""){
-        if (name == "" && password == ""){
-            alert("Anna käyttäjänimi ja salasana")
+// Näyttää virheet, jos kentissä ei ole mitään
+function errors(name, password, email, locality){
+    if (name == "" || password == "" || email == "" || locality == ""){
+        let errorMsg = []
+        if (name == ""){
+            errorMsg.push("käyttäjänimi")
         }
-        else if (name == ""){
-            alert("Anna käyttäjänimi")
+        if (password == ""){
+            errorMsg.push(" salasana")
         }
-        else if (password == ""){
-            alert("Anna salasana")
+        if (email == ""){
+            errorMsg.push(" sähköposti")
         }
+        if (locality == ""){
+            errorMsg.push(" paikkakunta")
+        }
+        alert("Anna " + errorMsg)
     }
 }
 
@@ -70,7 +81,7 @@ function checkInfo(){
     errors(name, password)
 
     if (name in userList && Object.keys(userList).length != 0){
-        if (password != userList[name]){
+        if (password != userList[name][0]){
             alert("Väärä salasana, yritä uudelleen")
         }
         else{
